2f169a0a1d28b60add1c7883a1571a4563a0c209
Sanish Thapa <thapasanish73@gmail.com>
2022-09-27 23:16:41 +0545

    static end

--- Basics/Basics/Program.cs
+++ Basics/Basics/Program.cs
@@ -9,6 +9,38 @@ using System.Threading.Tasks;
 
 namespace Basics
 {
+    //static
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var result = Calculator.Sum(10, 25); // calling static method
+            Calculator.Store(result);
+            Console.WriteLine(result);
+
+            var calcType = Calculator.Type; // accessing static variable
+            Calculator.Type = "Scientific"; // assign value to static variable
+        }
+    }
+
+    public static class Calculator
+    {
+        private static int _resultStorage = 0;
+
+        public static string Type = "Arithmetic";
+
+        public static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static void Store(int result)
+        {
+            _resultStorage = result;
+        }
+    }
 
     /*
 //interface
