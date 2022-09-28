19b933d60fa1113b3a2e753a7980d762d72c58ec
Sanish Thapa <thapasanish73@gmail.com>
2022-09-28 22:53:48 +0545

    ststic end

--- Basics/Basics/Program.cs
+++ Basics/Basics/Program.cs
@@ -9,76 +9,87 @@ using System.Threading.Tasks;
 
 namespace Basics
 {
-    //static
-    using System;
 
-    public class Program
-    {
-        public static void Main()
-        {
-            var result = Calculator.Sum(10, 25); // calling static method
-            Calculator.Store(result);
-            Console.WriteLine(result);
 
-            var calcType = Calculator.Type; // accessing static variable
-            Calculator.Type = "Scientific"; // assign value to static variable
-        }
-    }
-
-    public static class Calculator
-    {
-        private static int _resultStorage = 0;
-
-        public static string Type = "Arithmetic";
-
-        public static int Sum(int num1, int num2)
-        {
-            return num1 + num2;
-        }
-
-        public static void Store(int result)
-        {
-            _resultStorage = result;
-        }
-    }
 
     /*
+  //static
+  using System;
+
+  public class Program
+  {
+      public static void Main()
+      {
+          var result = Calculator.Sum(10, 25); // calling static method
+          Calculator.Store(result);
+          Console.WriteLine(result);
+
+
+
+          Calculator.Type = "Scientific"; // assign value to static variable
+          var calcType = Calculator.Type; // accessing static variable
+          Console.WriteLine(calcType);
+
+
+
+
+      }
+  }
+
+  public static class Calculator
+  {
+      private static int _resultStorage = 0;
+
+      public static string Type = "Arithmetic";
+
+      public static int Sum(int num1, int num2)
+      {
+          return num1 + num2;
+      }
+
+      public static void Store(int result)
+      {
+          _resultStorage = result;
+      }
+  }
+
+
 //interface
 interface IFile
 {
-    void WritingFile(String text);
-    void ReadFile();
+  void WritingFile(String text);
+  void ReadFile();
 }
 
 class FileInfo : IFile 
 {
-     void IFile.ReadFile()
-    {
-        Console.WriteLine("Reading File");
-    }
+   void IFile.ReadFile()
+  {
+      Console.WriteLine("Reading File");
+  }
 
-    void IFile.WritingFile(String text)
-    {
-        Console.WriteLine("Reading File");
-    }
+  void IFile.WritingFile(String text)
+  {
+      Console.WriteLine("Reading File");
+  }
 
-    public void searching()
-    {
-        Console.WriteLine("Searching File");
-    }
+  public void searching()
+  {
+      Console.WriteLine("Searching File");
+  }
 }
 
 class Program
 {
-    public static void Main()
-    {
-        FileInfo file = new FileInfo();
-        IFile file2 = new FileInfo();
+  public static void Main()
+  {
+      FileInfo file = new FileInfo();
+      IFile file2 = new FileInfo();
 
-        file2.WritingFile("content");
-        file.searching();
+      file2.WritingFile("content");
+      file.searching();
 
-    }
+  }
 }
 
 
@@ -86,318 +97,318 @@ class Program
 //Nullable value
 class Program
 {
-   public static void Main()
-   {
-       Nullable<int> i = null;
+ public static void Main()
+ {
+     Nullable<int> i = null;
 
-       if(i.HasValue)
+     if(i.HasValue)
 
-           Console.WriteLine(i.Value);
+         Console.WriteLine(i.Value);
 
 
-       else
-           Console.WriteLine("null");
+     else
+         Console.WriteLine("null");
 
-   }
+ }
 }
 
 
-    //dynamic types
-    class Program
-    {
-        public static void Main()
-        {
+  //dynamic types
+  class Program
+  {
+      public static void Main()
+      {
 
-            dynamic num = 100;
-            Console.WriteLine("Value: {0}, Type: {1}", num, num.GetType());
-
-
-            num = "sanish";
-            Console.WriteLine("Value: {0}, Type: {1}", num, num.GetType());
+          dynamic num = 100;
+          Console.WriteLine("Value: {0}, Type: {1}", num, num.GetType());
 
 
-            num = false;
-            Console.WriteLine("Value: {0}, Type: {1}", num, num.GetType());
-
-        }
-    }
+          num = "sanish";
+          Console.WriteLine("Value: {0}, Type: {1}", num, num.GetType());
 
 
-           // Anonymous Type
+          num = false;
+          Console.WriteLine("Value: {0}, Type: {1}", num, num.GetType());
 
-          public  class Student
-           {
-               public int Id { get; set; }
-               public string Name { get; set; }
-               public int Class { get; set; }
-           }
-
-           class Program
-           {
-               public static void Main()
-               {
-                   IList<Student> studentList = new List<Student>()
-                   {
-                         new Student() { Id = 1 ,  Name = "sanish" , Class = 10 },
-                   new Student() { Id = 2, Name = "Samundra", Class = 11 },
-                   new Student() { Id = 3, Name = "sonima", Class = 12 },
-                   new Student() { Id = 4, Name = "nirik", Class = 13 }
-                   };
-
-                   var students = from s in studentList
-                                  select s;
-
-                   Console.WriteLine(students);
-
-                                //  select new(id = s.Id, name = s.Name, clas = Class);
+      }
+  }
 
 
-               }
-           }
+         // Anonymous Type
 
-        class Builder
-        {
-            public static void Main()
-            {
-                //string builder
-                StringBuilder sb = new StringBuilder();
-                sb.Append("hi");
-                sb.AppendLine("sanish");
-                sb.AppendLine("Nice To Meet You!!!");
-
-                Console.Write(sb);
-
-
-            }
-
-
-        }
-
-
-
-        //enum 
-        class eenum
-        {
-            enum value_no
-            {
-                one = 5,
-                two = 2,
-                three = 3,
-                four = 4,
-                five = 50
-            }
-
-            public static void Main()
-            {
-               // Console.WriteLine((int)value_no.one);
-
-                var value = (value_no)50;
-                Console.WriteLine(value);
-            }
-        }
-
-
-
-
-        //Static member of a class
-
-        static class person
-        {
-            public static int id;
-            public static string name;
-            public static int age;
-
-            public static void Display()
-            {
-                Console.WriteLine("ID is: " + id + " and you name is:" + name + " and age is :" + age);
-            }
-        }
-
-        class program
-        {
-            static void Main()
-            {
-                person.id = 1;
-                person.name = "sanish";
-                person.age = 21;
-
-                person.Display();
-            }
-        }
-
-        //Argumnet List
-
-        class program
-        {
-            class IndexerClass
-            {
-                private string[] names = new string[10];
-
-                public string this[int i]
-                {
-                    get
-                    {
-                        return names[i];
-                    }
-                    set
-                    {
-                        names[i] = value;
-                    }
-                }
-            }
-
-            static void Main()
-            {
-                IndexerClass ic = new IndexerClass();
-                ic[0] = "sanish";
-                ic[1] = "karan";
-                ic[2] = "some";
-                ic[3] = "s";
-                ic[4] = "h";
-                ic[5] = "n";
-                ic[6] = "ssdf";
-
-                for(int i = 0; i < 10; i++)
-                {
-                    Console.WriteLine(ic[i]);
-                }
-
-            }
-        }
-
-        //Automatic propertie Get/set
-
-        class demo
-        {
-            public int a {get; set; }
-
-            public int b { get; set; }
-
-            public int sum
-            {
-                get
-                {
-                    return a + b;
-                }
-            }
-        }
-
-        class test
-        {
-            public static void Main()
-            {
-                demo demo = new demo();
-                demo.a = 1;
-                demo.b = 2;
-                Console.WriteLine("sum of " + demo.a + " and " + demo.b + " = " + demo.sum);
-            }
-        }
-
-         * 
-        class pattern
-        {
-           //Static Constructor
-
-           public pattern()
-           {
-               Console.WriteLine("I am not static  constructor");
-           }
-
-           static pattern()
-           {
-               Console.WriteLine("I am  static  constructor");
-           }
-        }
-
-        class program
-        {
-
-           public static void Main()
-           {
-               new pattern();
-               //new pattern();
-           }
-        }
-
-
-          //heart pattern
-        public static void Main()
-        {
-         int fore = 4;
-         int fore2 = 4;
-         int ten = 8;
-         int twientyOne = 22;
-
-         for (int i = 0; i < 3; i++)
+        public  class Student
          {
-             for (int j = 3; j > i; j--)
-             {
-                 Console.Write(" ");
-             }
-
-             for(int k = 0; k < fore; k++)
-             {
-
-                 Console.Write("*");
-             }
-
-             fore = fore + 3;
-
-             for(int l = 1; l<ten; l++)
-             {
-                 Console.Write(" ");
-             }
-
-             ten = ten - 3;
-
-             for (int k = 0; k < fore2; k++)
-             {
-
-                 Console.Write("*");
-             }
-             fore2 = fore2 + 3;
-
-
-
-             Console.WriteLine();
+             public int Id { get; set; }
+             public string Name { get; set; }
+             public int Class { get; set; }
          }
 
-         for(var i= 1; i<12; i++)
+         class Program
          {
-             for(var j = 0; j<i; j++)
+             public static void Main()
              {
-                 Console.Write(" ");
-             }
+                 IList<Student> studentList = new List<Student>()
+                 {
+                       new Student() { Id = 1 ,  Name = "sanish" , Class = 10 },
+                 new Student() { Id = 2, Name = "Samundra", Class = 11 },
+                 new Student() { Id = 3, Name = "sonima", Class = 12 },
+                 new Student() { Id = 4, Name = "nirik", Class = 13 }
+                 };
 
-             for(var k = 1; k<twientyOne; k++)
-             {
-                 Console.Write("*");
-             }
-             twientyOne = twientyOne - 2;
+                 var students = from s in studentList
+                                select s;
 
-             Console.WriteLine();
+                 Console.WriteLine(students);
+
+                              //  select new(id = s.Id, name = s.Name, clas = Class);
+
+
+             }
          }
 
+      class Builder
+      {
+          public static void Main()
+          {
+              //string builder
+              StringBuilder sb = new StringBuilder();
+              sb.Append("hi");
+              sb.AppendLine("sanish");
+              sb.AppendLine("Nice To Meet You!!!");
+
+              Console.Write(sb);
 
 
-            int four = 4;
-            for(int i = 0; i<3; i++)
-            {
-                for(int j= 3; j>i; j--)
-                {
-                    Console.Write("0");
-                }
-            //    Console.WriteLine();
-            for(int k = 1; k<=four; k++)
-                {
-                    four = four + 2;
+          }
 
-                    Console.Write("*");
-                }
-                Console.WriteLine();
-            }
-        }
-        }*/
+
+      }
+
+
+
+      //enum 
+      class eenum
+      {
+          enum value_no
+          {
+              one = 5,
+              two = 2,
+              three = 3,
+              four = 4,
+              five = 50
+          }
+
+          public static void Main()
+          {
+             // Console.WriteLine((int)value_no.one);
+
+              var value = (value_no)50;
+              Console.WriteLine(value);
+          }
+      }
+
+
+
+
+      //Static member of a class
+
+      static class person
+      {
+          public static int id;
+          public static string name;
+          public static int age;
+
+          public static void Display()
+          {
+              Console.WriteLine("ID is: " + id + " and you name is:" + name + " and age is :" + age);
+          }
+      }
+
+      class program
+      {
+          static void Main()
+          {
+              person.id = 1;
+              person.name = "sanish";
+              person.age = 21;
+
+              person.Display();
+          }
+      }
+
+      //Argumnet List
+
+      class program
+      {
+          class IndexerClass
+          {
+              private string[] names = new string[10];
+
+              public string this[int i]
+              {
+                  get
+                  {
+                      return names[i];
+                  }
+                  set
+                  {
+                      names[i] = value;
+                  }
+              }
+          }
+
+          static void Main()
+          {
+              IndexerClass ic = new IndexerClass();
+              ic[0] = "sanish";
+              ic[1] = "karan";
+              ic[2] = "some";
+              ic[3] = "s";
+              ic[4] = "h";
+              ic[5] = "n";
+              ic[6] = "ssdf";
+
+              for(int i = 0; i < 10; i++)
+              {
+                  Console.WriteLine(ic[i]);
+              }
+
+          }
+      }
+
+      //Automatic propertie Get/set
+
+      class demo
+      {
+          public int a {get; set; }
+
+          public int b { get; set; }
+
+          public int sum
+          {
+              get
+              {
+                  return a + b;
+              }
+          }
+      }
+
+      class test
+      {
+          public static void Main()
+          {
+              demo demo = new demo();
+              demo.a = 1;
+              demo.b = 2;
+              Console.WriteLine("sum of " + demo.a + " and " + demo.b + " = " + demo.sum);
+          }
+      }
+
+       * 
+      class pattern
+      {
+         //Static Constructor
+
+         public pattern()
+         {
+             Console.WriteLine("I am not static  constructor");
+         }
+
+         static pattern()
+         {
+             Console.WriteLine("I am  static  constructor");
+         }
+      }
+
+      class program
+      {
+
+         public static void Main()
+         {
+             new pattern();
+             //new pattern();
+         }
+      }
+
+
+        //heart pattern
+      public static void Main()
+      {
+       int fore = 4;
+       int fore2 = 4;
+       int ten = 8;
+       int twientyOne = 22;
+
+       for (int i = 0; i < 3; i++)
+       {
+           for (int j = 3; j > i; j--)
+           {
+               Console.Write(" ");
+           }
+
+           for(int k = 0; k < fore; k++)
+           {
+
+               Console.Write("*");
+           }
+
+           fore = fore + 3;
+
+           for(int l = 1; l<ten; l++)
+           {
+               Console.Write(" ");
+           }
+
+           ten = ten - 3;
+
+           for (int k = 0; k < fore2; k++)
+           {
+
+               Console.Write("*");
+           }
+           fore2 = fore2 + 3;
+
+
+
+           Console.WriteLine();
+       }
+
+       for(var i= 1; i<12; i++)
+       {
+           for(var j = 0; j<i; j++)
+           {
+               Console.Write(" ");
+           }
+
+           for(var k = 1; k<twientyOne; k++)
+           {
+               Console.Write("*");
+           }
+           twientyOne = twientyOne - 2;
+
+           Console.WriteLine();
+       }
+
+
+
+          int four = 4;
+          for(int i = 0; i<3; i++)
+          {
+              for(int j= 3; j>i; j--)
+              {
+                  Console.Write("0");
+              }
+          //    Console.WriteLine();
+          for(int k = 1; k<=four; k++)
+              {
+                  four = four + 2;
+
+                  Console.Write("*");
+              }
+              Console.WriteLine();
+          }
+      }
+      }*/
 
 
     /*
